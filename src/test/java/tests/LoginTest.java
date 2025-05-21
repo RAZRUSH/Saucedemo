f920d77cf5a35349dbf004b4f58ca56cb7f0f616
Rushan Razyapov <825613hefy@gmail.com>
2025-05-21 20:14:47 +0300

    login test3

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.beust.ah.A;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,30 +9,25 @@ public class LoginTest extends BaseTest {
 
     @Test
 
-    public void loginTest1() {
+    public void checkLoginWithWrongPassword() {
         loginPage.open();
         loginPage.login("standard_user", "123456789");
         Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Username and password do not match any user in this service");
-
-
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("123456789");
-        driver.findElement(By.id("login-button")).click();
-        String message = driver.findElement(By.cssSelector("[data-test=error]")).getText();
-        Assert.assertEquals(message, "Epic sadface: Username and password do not match any user in this service");
 
     }
 
     @Test
 
-    public void loginTest2() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("");
-        driver.findElement(By.id("login-button")).click();
-        String message = driver.findElement(By.cssSelector("[data-test=error]")).getText();
-        Assert.assertEquals(message, "Epic sadface: Password is required");
+    public void checkLoginWithEmptyPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "");
+        Assert.assertEquals(loginPage.getErrorMessage(), "Epic sadface: Password is required");
 
+    }
+    @Test
+    public void checkLoginWithValidCred(){
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        Assert.assertTrue(productsPage.titleIsDisplayed());
     }
 }
